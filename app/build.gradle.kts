109e75f7728eff0b20fb1e360ec67cf07bc08c3b
karshC <karshpsu@gmail.com>
2024-03-18 16:03:56 -0400

    adding all dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,9 +34,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
+    val lifecycle_version = "2.7.0"
+    val room_version = "2.6.1"
+    val coroutine_version = "1.8.1-Beta"
+    val dagger_version = "2.51"
+    val retrofit_version = "2.10.0"
+    val okhttp_version = "4.12.0"
+    val glide_version = "4.16.0"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +56,35 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+    // Annotation processor
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+    //Room
+    implementation("androidx.room:room-runtime:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+    //Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutine_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutine_version")
+    //Dagger
+    implementation("com.google.dagger:dagger:$dagger_version")
+    annotationProcessor("com.google.dagger:dagger-compiler:$dagger_version")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    //Gson convertor for Retrofit
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    //okhttp intercepter
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttp_version")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:$glide_version")
+    kapt("com.github.bumptech.glide:compiler:$glide_version")
+
 }
