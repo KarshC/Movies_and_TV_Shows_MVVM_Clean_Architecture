c2a4d00c55f73872fa44f97d44f7f88f771fd77f
karshC <karshpsu@gmail.com>
2024-03-19 12:39:16 -0400

    api service interface

---
+++ app/src/main/java/com/example/moviesandtvshows/data/api/TMDBService.kt
@@ -0,0 +1,23 @@
+package com.example.moviesandtvshows.data.api
+
+import com.example.moviesandtvshows.data.MoviePopularList
+import com.example.moviesandtvshows.data.PeoplePopularList
+import com.example.moviesandtvshows.data.TvPopularList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("page") page: Int = 1
+    ): Response<MoviePopularList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(@Query("api_key") apiKey: String): Response<TvPopularList>
+
+    @GET("person/popular")
+    suspend fun getPopularPeople(@Query("api_key") apiKey: String): Response<PeoplePopularList>
+}
