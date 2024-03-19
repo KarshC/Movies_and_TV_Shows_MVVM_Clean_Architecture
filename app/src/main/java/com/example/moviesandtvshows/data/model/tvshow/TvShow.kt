834e51737b5d51ab6b05994d15fe60021626063e
karshC <karshpsu@gmail.com>
2024-03-19 13:06:45 -0400

    adding Entities for room and some refactoring

--- app/src/main/java/com/example/moviesandtvshows/data/model/tvshow/TvShow.kt
+++ app/src/main/java/com/example/moviesandtvshows/data/model/tvshow/TvShow.kt
@@ -1,13 +1,17 @@
 package com.example.moviesandtvshows.data.model.tvshow
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "popular_tv_shows")
 data class TvShow(
-    @SerializedName("first_air_date")
-    val firstAirDate: String?,
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
+    @SerializedName("first_air_date")
+    val firstAirDate: String?,
     @SerializedName("name")
     val name: String?,
     @SerializedName("overview")
