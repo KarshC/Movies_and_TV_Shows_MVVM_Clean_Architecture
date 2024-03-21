b855edec53e37215081f30f604eb100976e04350
karshC <karshpsu@gmail.com>
2024-03-21 14:18:54 -0400

    click listener at home activity

--- app/src/main/java/com/example/moviesandtvshows/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/moviesandtvshows/presentation/HomeActivity.kt
@@ -1,15 +1,29 @@
 package com.example.moviesandtvshows.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.moviesandtvshows.R
 import com.example.moviesandtvshows.databinding.ActivityHomeBinding
+import com.example.moviesandtvshows.presentation.artist.ArtistActivity
+import com.example.moviesandtvshows.presentation.movie.MovieActivity
+import com.example.moviesandtvshows.presentation.tvshow.TvShowActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        dataBinding.movieButton.setOnClickListener {
+            val intent = Intent(this, MovieActivity::class.java)
+            startActivity(intent)
+        }
+        dataBinding.tvButton.setOnClickListener {
+            startActivity(Intent(this, TvShowActivity::class.java))
+        }
+        dataBinding.artistButton.setOnClickListener {
+            startActivity(Intent(this, ArtistActivity::class.java))
+        }
     }
 }
