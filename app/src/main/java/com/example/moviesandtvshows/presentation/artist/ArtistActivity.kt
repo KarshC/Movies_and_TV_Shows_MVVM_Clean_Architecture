3a6d86ee2009a07e2380b0fc985b51ce787dfba0
karshC <karshpsu@gmail.com>
2024-03-21 12:58:03 -0400

    adding activities

---
+++ app/src/main/java/com/example/moviesandtvshows/presentation/artist/ArtistActivity.kt
@@ -0,0 +1,16 @@
+package com.example.moviesandtvshows.presentation.artist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.moviesandtvshows.R
+
+class ArtistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist)
+
+    }
+}
---
+++ app/src/main/java/com/example/moviesandtvshows/presentation/tvshow/TvShowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviesandtvshows.presentation.tvshow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.moviesandtvshows.R
+
+class TvShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tv_show)
+    }
+}
