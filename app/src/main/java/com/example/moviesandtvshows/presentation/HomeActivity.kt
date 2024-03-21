41bbe4749bd35635deb7924d2a35ab170bd0527b
karshC <karshpsu@gmail.com>
2024-03-21 12:16:19 -0400

    updating home activity with data binding

--- app/src/main/java/com/example/moviesandtvshows/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/moviesandtvshows/presentation/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.example.moviesandtvshows.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.moviesandtvshows.R
+import com.example.moviesandtvshows.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
